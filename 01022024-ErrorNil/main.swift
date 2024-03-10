93e2ef85db853ae1ccaff3651aeebe787fedd193
ShAglarov <shamil.aglarov@gmail.com>
2024-03-10 22:27:56 +0300

    first version fighterClub

--- 01022024-ErrorNil/main.swift
+++ 01022024-ErrorNil/main.swift
@@ -2,350 +2,210 @@
 
 import Foundation
 
-enum Models {
-    case BMW
-    case Mersedes
+
+protocol UltimateAbility {
+    func ultimateAbility() -> Int
 }
-enum LoadUnloadCapable: CustomStringConvertible {
-    case load
-    case unload
+
+protocol IsDeadDelegate {
+    func isDead()
+}
+
+enum TypeFighter: CustomStringConvertible, CaseIterable {
+    case warrior
+    case dodger
+    case ironMan
+    
+    var multiply: (strength: Int, vitality: Int, agility: Int) {
+        switch self {
+        case .warrior: (10, 10, 2)
+        case .dodger: (5, 5, 10)
+        case .ironMan: (7,5,4)
+        }
+    }
     
     var description: String {
         switch self {
-        case .load:
-            return "загрузить"
-        case .unload:
-            return "выгрузить"
+        case .warrior: "Воин"
+        case .dodger: "Ловкач"
+        case .ironMan: "Железный человек"
+        }
+    }
+    
+    var ultimateAbilityDescription: String {
+        switch self {
+        case .warrior: "впадает в ярость и бьет щитом 3 раза"
+        case .dodger: "уворачивается от удара, и бьет ответный удар"
+        case .ironMan: "может нанести супер удар и при этом теряет собственное здоровье"
+        }
+    }
+    
+    // имя бойца
+    private func namingFighter() -> String {
+        print("Введите имя для своего боца")
+        guard let nameFighter = readLine(),
+              nameFighter != "" else { return namingFighter() }
+        return nameFighter
+    }
+    
+    var selectedType: Fighter {
+        switch self {
+        case .warrior: Warrior(name: namingFighter())
+        case .dodger: Dodger(name: namingFighter())
+        case .ironMan: IronMan(name: namingFighter())
         }
     }
 }
 
-enum ActionWithCar {
-    case startEnigine
-    case stopEnigine
-    case startMooving
-    case stopMooving
-    case loadUnload(_ : LoadUnloadCapable, volume: Float)
-    case openDoors
-    case closeDoors
-    case openTrunk
-    case closeTrunk
-    //    case openWindows
-    //    case closeWindows
+class Fighter {
     
-}
-
-protocol Vehicle {
-    var carBrand: Models { get set }
-    var year: String { get set }
-    var volumeTrunk: Float { get }
-    var сurrentVolume: Float { get set }
-    var startEnigine: Bool { get set }
-    var stopEnigine: Bool { get set }
-    var startMooving: Bool { get set }
-    var stopMooving: Bool { get set }
-    var openDoors: Bool { get set }
-    var closeDoors: Bool { get set }
-    var openTrunk: Bool { get set }
-    var closeTrunk: Bool { get set }
-}
-
-protocol VehicleAction {
-    var successMessage: String { get set }
-    var failureMessage: String { get set }
+    let name: String
+    var type: TypeFighter
     
-    mutating func canPerform(vehicle: Vehicle) -> Bool
-    func performAction(on vehicle: inout Vehicle)
-}
-
-extension VehicleAction {
-    mutating func perform(vehicle: inout Vehicle) {
-        if canPerform(vehicle: vehicle) {
-            performAction(on: &vehicle)
-            print(successMessage)
-        } else {
-            print(failureMessage)
+    private var _strength = Int()
+    var strength: Int {
+        get { return _strength }
+        set {
+            _strength = newValue
+            damage = _strength * type.multiply.strength
         }
     }
-}
-
-protocol VehicleActionParameters {
-    var successMessage: String { get set }
-    var failureMessage: String { get set }
-    mutating func canPerform(on vehicle: Vehicle, action: LoadUnloadCapable, volume: Float) -> Bool
-    mutating func performAction(on vehicle: inout Vehicle, action: LoadUnloadCapable, volume: Float)
-}
-extension VehicleActionParameters {
-    mutating func perform(on vehicle: inout Vehicle, action: LoadUnloadCapable, volume: Float) {
-        if canPerform(on: vehicle, action: action, volume: volume) {
-            performAction(on: &vehicle, action: action, volume: volume)
-            print(successMessage)
-        } else {
-            print(failureMessage)
+    private var _vitality = Int()
+    var vitality: Int {
+        get { return _vitality }
+        set {
+            _vitality = newValue
+            health = vitality * type.multiply.vitality
         }
     }
-}
-
-struct StartMoovingAction: VehicleAction {
-    var successMessage: String = "Автомобиль тронулся"
-    var failureMessage: String = "Автомобиль уже в движении"
-    
-    mutating func canPerform(vehicle: Vehicle) -> Bool {
-        guard !vehicle.openDoors else {
-            failureMessage = "Прежде чем тронуться, закройте все двери"
-            return false
+    private var _agility = Int()
+    var agility: Int {
+        get { return _agility}
+        set {
+            _agility = newValue
+            chance = _agility * type.multiply.agility
         }
-        guard !vehicle.openTrunk else {
-            failureMessage = "Прежде чем тронуться, закройте багажник"
-            return false
-        }
-        if !vehicle.startEnigine {
-            failureMessage = "Необходимо сначала завести автомобиль"
-            return false
-        }
-        guard !vehicle.startMooving else {
-            return false
-        }
-        
-        return !vehicle.startMooving
     }
     
-    func performAction(on vehicle: inout Vehicle) {
-        vehicle.startMooving = true
-    }
-}
-
-struct StopMoovingAction: VehicleAction {
-    var successMessage: String = "Автомобиль остановился"
-    var failureMessage: String = "Автомобиль и так стоит на месте"
+    var delegate: IsDeadDelegate?
     
-    func canPerform(vehicle: Vehicle) -> Bool {
-        return !vehicle.stopMooving
-    }
-    
-    func performAction(on vehicle: inout Vehicle) {
-        vehicle.stopMooving = true
-        vehicle.startMooving = false
-    }
-}
-
-struct StartEnigineAction: VehicleAction {
-    var successMessage: String = "Автомобиль завелся"
-    var failureMessage: String = "Автомобиль был ранее заведен"
-    
-    func canPerform(vehicle: Vehicle) -> Bool {
-        return !vehicle.startEnigine
-    }
-    
-    func performAction(on vehicle: inout Vehicle) {
-        vehicle.startEnigine = true
-        vehicle.stopEnigine = false
-    }
-}
-
-struct StopEnigineAction: VehicleAction {
-    var successMessage: String = "Мотор заглушен"
-    var failureMessage: String = "Мотор ранее уже был залушен"
-    
-    mutating func canPerform(vehicle: Vehicle) -> Bool {
-        if vehicle.startMooving {
-            failureMessage = "Прежде чем загулшить мотор, притормозите где ни будь"
-            return false
-        }
-        return !vehicle.stopEnigine
-    }
-    
-    func performAction(on vehicle: inout Vehicle) {
-        vehicle.stopEnigine = true
-        vehicle.startEnigine = false
-    }
-}
-
-struct OpenDoorAction: VehicleAction {
-    var successMessage: String = "Вы открыли двери автомобиля"
-    var failureMessage: String = ""
-    
-    mutating func canPerform(vehicle: Vehicle) -> Bool {
-        guard !vehicle.startMooving else {
-            failureMessage = "Нельзя открыть двери на ходу, чтобы открыть двери, притормозите где-нибдь"
-            return false
-        }
-        guard !vehicle.openDoors else {
-            failureMessage = "Нельзя открыть двери, так как они уже открыты"
-            return false
-        }
-        return true
-    }
-    
-    func performAction(on vehicle: inout Vehicle) {
-        vehicle.openDoors = true
-        vehicle.closeDoors = false
-    }
-}
-
-struct CloseDoorAction: VehicleAction {
-    var successMessage: String = "Вы закрыли двери автомобиля"
-    var failureMessage: String = "Нельзя закрыть двери, так как они уже закрыты"
-    
-    mutating func canPerform(vehicle: Vehicle) -> Bool {
-        guard !vehicle.closeDoors else {
-            return false
-        }
-        return true
-    }
-    
-    func performAction(on vehicle: inout Vehicle) {
-        vehicle.closeDoors = true
-        vehicle.openDoors = false
-    }
-}
-
-struct OpenTrunkAction: VehicleAction {
-    var successMessage: String = "Вы открыли багажник"
-    var failureMessage: String = ""
-    
-    mutating func canPerform(vehicle: Vehicle) -> Bool {
-        guard !vehicle.openTrunk else {
-            failureMessage = "Нельзя открыть багажник, так как багажник уже открыт"
-            return false
-        }
-        guard !vehicle.startMooving else {
-            failureMessage = "Нельзя открыть багажник на ходу, чтобы открыть багажник, притормозите где-нибдь"
-            return false
-        }
-        return true
-    }
-    
-    func performAction(on vehicle: inout Vehicle) {
-        vehicle.openTrunk = true
-        vehicle.closeTrunk = false
-    }
-}
-
-struct CloseTrunkAction: VehicleAction {
-    var successMessage: String = "Вы закрыли багажник автомобиля"
-    var failureMessage: String = "Нельзя закрыть багажник, так как багажник уже закрыт"
-    
-    mutating func canPerform(vehicle: Vehicle) -> Bool {
-        guard !vehicle.closeTrunk else {
-            return false
-        }
-        return true
-    }
-    
-    func performAction(on vehicle: inout Vehicle) {
-        vehicle.closeTrunk = true
-        vehicle.openTrunk = false
-    }
-}
-
-struct LoadUnloadAction: VehicleActionParameters {
-    
-    var successMessage: String = ""
-    var failureMessage: String = ""
-    
-    mutating func canPerform(on vehicle: Vehicle, action: LoadUnloadCapable, volume: Float) -> Bool {
-        guard !vehicle.startMooving else {
-            failureMessage = "Автомобиль в движении, сначала остановитесь где ни будь"
-            return false
-        }
-        guard vehicle.openTrunk else {
-            failureMessage = "Чтобы \(action) в багаж, сначала откройте багажник"
-            return false
-        }
-        
-        switch action {
-        case .load:
-            if vehicle.сurrentVolume + volume > vehicle.volumeTrunk {
-                let spaceInTrunk = vehicle.volumeTrunk - vehicle.сurrentVolume
-                failureMessage = "В багажнике нет столько места, вы можете загрузить только \(spaceInTrunk) л."
-                return false
+    var damage = Int()
+    private var _health = Int()
+    var health: Int {
+        get { return _health }
+        set {
+            _health = newValue
+            if _health <= 0 {
+                delegate?.isDead()
             }
-            let residue = vehicle.volumeTrunk - (vehicle.сurrentVolume + volume)
-            successMessage = "Груз объемом \(volume) л успешно загружен! В багажнике осталось \(residue) л свободного места."
-        case .unload:
-            if volume > vehicle.сurrentVolume {
-                failureMessage = "Невозможно выгрузить столько, вы можете выгрузить лишь \(vehicle.сurrentVolume) л."
-                return false
-            }
-            let residue = vehicle.сurrentVolume - volume
-            successMessage = "Груз объемом \(volume) л успешно выгружен! В багажнике осталось \(residue) л свободного места."
         }
-        return true
     }
+    var chance = Int()
     
-    mutating func performAction(on vehicle: inout Vehicle, action: LoadUnloadCapable, volume: Float) {
-        switch action {
-        case .load:
-            vehicle.сurrentVolume += volume
-        case .unload:
-            vehicle.сurrentVolume -= volume
-        }
+    internal init(name: String, type: TypeFighter, strength: Int, vitality: Int, agility: Int) {
+        self.name = name
+        self.type = type
+        self.vitality = vitality
+        self.agility = agility
+        self.strength = strength
     }
 }
 
-struct VehicleActionHandler {
-    func performAction(_ action: ActionWithCar, vehicle: inout Vehicle) {
-        switch action {
-        case .startMooving:
-            var startVoogingAction = StartMoovingAction()
-            startVoogingAction.perform(vehicle: &vehicle)
-        case .stopMooving:
-            var stopMoogingAction = StopMoovingAction()
-            stopMoogingAction.perform(vehicle: &vehicle)
-        case .startEnigine:
-            var startEnigineAction = StartEnigineAction()
-            startEnigineAction.perform(vehicle: &vehicle)
-        case .stopEnigine:
-            var stopEnigineAction = StopEnigineAction()
-            stopEnigineAction.perform(vehicle: &vehicle)
-        case .loadUnload(let action, let volume):
-            var loadUnloadAction = LoadUnloadAction()
-            loadUnloadAction.perform(on: &vehicle, action: action, volume: volume)
-        case .openDoors:
-            var openDoorsAction = OpenDoorAction()
-            openDoorsAction.perform(vehicle: &vehicle)
-        case .closeDoors:
-            var closeDoorsAction = CloseDoorAction()
-            closeDoorsAction.perform(vehicle: &vehicle)
-        case .openTrunk:
-            var openTrunkAction = OpenTrunkAction()
-            openTrunkAction.perform(vehicle: &vehicle)
-        case .closeTrunk:
-            var closeTrunkAction = CloseTrunkAction()
-            closeTrunkAction.perform(vehicle: &vehicle)
-        }
+final class Warrior: Fighter, UltimateAbility {
+    
+    init(name: String) {
+        super.init(name: name, 
+                   type: .warrior,
+                   strength: 10,
+                   vitality: 10,
+                   agility: 0)
+    }
+    
+    func ultimateAbility() -> Int {
+        return damage * 3
     }
 }
 
-struct Car: Vehicle {
+final class Dodger: Fighter, UltimateAbility {
     
-    var carBrand: Models
-    var year: String
+    init(name: String) {
+        super.init(name: name,
+                   type: .dodger,
+                   strength: 5,
+                   vitality: 5,
+                   agility: 10)
+    }
     
-    var volumeTrunk: Float
-    var сurrentVolume: Float = 0
-    
-    var startEnigine: Bool = false
-    var stopEnigine: Bool = true
-    var startMooving: Bool = false
-    var stopMooving: Bool = false
-    var openDoors: Bool = false
-    var closeDoors: Bool = true
-    var openTrunk: Bool = false
-    var closeTrunk: Bool = true
+    func ultimateAbility() -> Int {
+        return 10
+    }
 }
 
-var car: Vehicle = Car(carBrand: .Mersedes, year: "2024", volumeTrunk: 100)
+final class IronMan: Fighter, UltimateAbility {
+    
+    init(name: String) {
+        super.init(name: name,
+                   type: .ironMan,
+                   strength: 5,
+                   vitality: 5,
+                   agility: 10)
+    }
+    
+    func ultimateAbility() -> Int {
+        return 10
+    }
+}
 
+final class Game {
+    private var player1: Fighter?
+    private var player2: Fighter?
+    
+    var isGameOver: Bool = false
+    var round: Int = 1
+    
+    func startGame() {
+        print("ИГРОК 1 СОЗДАЕТ БОЙЦА")
+        player1 = selectedType()
+        player1?.delegate = self
+        print("ИГРОК 2 СОЗДАЕТ БОЙЦА")
+        player2 = selectedType()
+        player2?.delegate = self
+    }
+    
+    func startFighting() {
+        while !isGameOver {
+            print("Для старта игры нажмите Enter", terminator: " ")
+            let _ = readLine()
+            print("Раунд - \(round)")
+        }
+    }
+    
+    // устанавливаем тип бойца
+    private func typeFighter() -> TypeFighter {
+        print("Выберите бойца ")
+        TypeFighter.allCases.enumerated().map { "\($0.offset+1): \($0.element)"}.forEach { print($0) }
+        
+        guard let indexTypeFighter = Int(readLine() ?? "0"), indexTypeFighter <= TypeFighter.allCases.count else { return typeFighter() }
+        return TypeFighter.allCases[indexTypeFighter-1]
+    }
+    // выбираем бойца
+    func selectedType() -> Fighter {
+        let typeFighter = typeFighter()
 
-VehicleActionHandler().performAction(.openTrunk, vehicle: &car)
-VehicleActionHandler().performAction(.loadUnload(.load, volume: 60), vehicle: &car)
-VehicleActionHandler().performAction(.loadUnload(.load, volume: 100), vehicle: &car)
-VehicleActionHandler().performAction(.loadUnload(.unload, volume: 40), vehicle: &car)
-VehicleActionHandler().performAction(.loadUnload(.unload, volume: 1), vehicle: &car)
-VehicleActionHandler().performAction(.loadUnload(.unload, volume: 60), vehicle: &car)
-VehicleActionHandler().performAction(.openDoors, vehicle: &car)
-VehicleActionHandler().performAction(.startEnigine, vehicle: &car)
-VehicleActionHandler().performAction(.closeDoors, vehicle: &car)
-VehicleActionHandler().performAction(.startMooving, vehicle: &car)
+        return typeFighter.selectedType
+    }
+    
+    func calculateDamage(agressor: Fighter, victim: Fighter) {
+        
+    }
+}
+
+extension Game: IsDeadDelegate {
+    func isDead() {
+        self.isGameOver = true
+    }
+}
+
+let game = Game()
+
+game.selectedType()
